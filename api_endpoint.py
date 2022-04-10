bbf1bef2a133957595978f332f4b00af64680b60
coetting <carsonoetting@gmail.com>
2022-04-10 16:44:02 -0600

    Updated json2 and api_endpoint

--- api_endpoint.py
+++ api_endpoint.py
@@ -1,9 +1,39 @@
 import json
-import SSL
+import ssl
 import urllib.request, urllib.parse, urllib.error
 
-serviceurl = input("URL address: ")
-if len(serviceurl) < 1:
-    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
-loc = input("What is the location you are trying to reach?: ")
+api_key = False
+
+if api_key is False:
+    api_key = 42
+    serviceurl = input("URL address: ")
+    if len(serviceurl) < 1:
+        serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+
+else:
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+
+while True:
+    address = input("What is the location you are trying to reach?: ")
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    if len(address) < 1:break
+    un = urllib.request.urlopen(url, context=ctx)
+    data = un.read().decode()
+
+    print(data)
