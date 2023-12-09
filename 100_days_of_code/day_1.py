93aeb52fd65381f934d4bd4f17f7f79e5501af0f
carsyno <carsonoetting@gmail.com>
2023-12-09 12:03:31 -0700

    Updated day 1 project in 100_days

---
+++ 100_days_of_code/day_1.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator.")
+city = input("What city did you grow up in?\n")
+pet = input("What was the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
+#band_name = city + " " + pet
+
+#print(band_name)
